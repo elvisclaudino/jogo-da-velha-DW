e5bfd9c57e6084f573a277768590a76082f57f82
Murilo Analiel Machado <muriloam@outlook.com>
2022-11-14 19:28:46 -0300

    Verificando se há alteraçoes

--- js/login/login.js
+++ js/login/login.js
@@ -1,36 +1,21 @@
 const form = {
   username: document.querySelector("#username"),
-  password: document.querySelector("password"),
+  password: document.querySelector("#password"),
+  submit: document.querySelector("#loginForm"),
 };
 
-let button = form.submit.addEventListener("click", (e) => {
+let button = form.submit.addEventListener("submit", (e) => {
   e.preventDefault();
-  const login = "http://localhost:3033/login";
 
-  fetch(login, {
-    method: "POST",
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: form.username.value,
-      password: form.password.value,
-    }),
+  axios.post('http://localhost:3033/users/login', {
+    username: form.username.value,
+    password: form.password.value
   })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      // code here //
-      if (data.error) {
-        alert("Error Password or Username"); /*displays error message*/
-      } else {
-        window.open(
-          "target.html"
-        ); /*opens the target page while Id & password matches*/
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
 });
